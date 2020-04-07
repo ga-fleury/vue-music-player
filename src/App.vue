020dc7251f952f5b31ac647a299e8554b0de6762
Gabriel Fleury <gabrielfleury95@gmail.com>
2020-04-06 21:11:15 -0300

    forward & previous working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MusicPlayer
+      :songList="songList"
+      :musicIndex="musicIndex"
+      @nextPressed="nextHandler"
+      @previousPressed="previousHandler"
+      @playPressed="playHandler"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import MusicPlayer from "./components/MusicPlayer.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    MusicPlayer
+  },
+  data() {
+    return {
+      playState: false,
+      musicIndex: 0,
+      songList: [
+        {
+          title: "Dissolve",
+          artist: "Absofacto",
+          duration: "3:44",
+          image: "https://i.imgur.com/Z9aaj5X.png"
+        },
+        {
+          title: "One More Time",
+          artist: "Marc Rebillet",
+          duration: "3:33",
+          image: "https://i.imgur.com/Xw2EieI.png"
+        },
+        {
+          title: "SUGAR",
+          artist: "BROCKHAMPTON",
+          duration: "3:24",
+          image: "https://i.imgur.com/KzJsVQH.png"
+        },
+        {
+          title: "Arty Boy - Tennyson Remix",
+          artist: "Flight Facilities, Emma Louise, Tennyson",
+          duration: "2:23",
+          image: "https://i.imgur.com/ZdWfa1C.jpg"
+        }
+      ]
+    };
+  },
+  methods: {
+    nextHandler() {
+      this.musicIndex === this.songList.length - 1
+        ? (this.musicIndex = 0)
+        : this.musicIndex++;
+    },
+    previousHandler() {
+      this.musicIndex === 0
+        ? (this.musicIndex = this.songList.length - 1)
+        : this.musicIndex--;
+    }
   }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "src/assets/css/app.scss";
 </style>
